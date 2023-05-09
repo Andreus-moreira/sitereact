7c164c14eae38bc1a349071bc822d7afa06fd1b3
Andreus-moreira <124805316+Andreus-moreira@users.noreply.github.com>
2023-05-09 20:35:40 -0300

    new

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+const App = () => {
+  return (
+    <div className="App">
+        <div className='gradient__bg'>
+          
+        </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/containers/index.js
@@ -0,0 +1,8 @@
+export { deafult as Blog } from './blog/Blog';
+export { deafult as Feature } from './feature/Feature';
+export { deafult as Footer } from './footer/Footer';
+export { deafult as Header } from './header/Header';
+export { deafult as Possibility } from './possibility/Possibility';
+export { deafult as WhatGPT3 } from './whatGPT3/WhatGPT3';
+
+
